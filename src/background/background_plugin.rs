a85e00bda95de9bdafaa5c304bfb2111d58a4a44
Vaaris5 <vaarischitkara16@gmail.com>
2026-08-19 21:24:40 +0530

    added a background plugin

---
+++ src/background/background_plugin.rs
@@ -0,0 +1,15 @@
+use bevy::prelude::*;
+
+use crate::BACKGROUND_COLOR;
+
+pub struct BackgroundPlugin;
+
+impl Plugin for BackgroundPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, set_bg);
+    }
+}
+
+fn set_bg(mut clear_color: ResMut<ClearColor>) {
+    clear_color.0 = BACKGROUND_COLOR;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use bevy::prelude::*;
+
+use crate::background::background_plugin::BackgroundPlugin;
+
+mod background;
+
+pub const BACKGROUND_COLOR: Color = Color::BLACK;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    resizable: false,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }),
+            BackgroundPlugin,
+        ))
+        .run();
 }
