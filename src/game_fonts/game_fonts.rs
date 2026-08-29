4c1d870bfa6f72f7e35837493141e35b55fc5730
Vaaris5 <vaarischitkara16@gmail.com>
2026-08-29 15:42:16 +0530

    added a start button

--- src/game_fonts/game_fonts.rs
+++ src/game_fonts/game_fonts.rs
@@ -1,6 +1,8 @@
 pub enum GameFonts {
     HyperspaceBold,
     ComfortaaBold,
+    ComfortaaMedium,
+    VectorBattle,
 }
 
 impl GameFonts {
@@ -8,6 +10,8 @@ impl GameFonts {
         match self {
             GameFonts::HyperspaceBold => "fonts/Hyperspace Font/Hyperspace Bold.otf",
             GameFonts::ComfortaaBold => "fonts/Comfortaa Font/static/Comfortaa-Bold.ttf",
+            GameFonts::ComfortaaMedium => "fonts/Comfortaa Font/static/Comfortaa-Medium.ttf",
+            GameFonts::VectorBattle => "fonts/Vector Battle Font/Vectorb.ttf",
         }
     }
 }
--- src/splashscreen/splash_screen_plugin.rs
+++ src/splashscreen/splash_screen_plugin.rs
@@ -1,34 +1,105 @@
-use crate::{GameState, game_fonts::game_fonts::GameFonts};
-use bevy::prelude::*;
+use crate::{
+    GameState::{self, SplashScreen},
+    game_fonts::game_fonts::GameFonts,
+};
+use bevy::{input::gamepad::GamepadButton::Start, prelude::*};
 
 pub struct SplashScreenPlugin;
 
 impl Plugin for SplashScreenPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::SplashScreen), spawn_splash_title);
+        app.add_systems(OnEnter(GameState::SplashScreen), splash_screen)
+            .add_systems(Update, button_interactions)
+            .add_systems(OnExit(GameState::SplashScreen), clean_up_splash_screen);
     }
 }
 
-fn spawn_splash_title(mut commands: Commands, assets_server: Res<AssetServer>) {
-    commands
-        .spawn(Node {
+#[derive(Component)]
+struct SplashScreenComponent;
+
+#[derive(Component)]
+struct StartButton;
+
+fn splash_screen(mut commands: Commands, assets_server: Res<AssetServer>) {
+    commands.spawn((
+        Node {
             width: percent(100),
             height: percent(100),
             justify_content: JustifyContent::Center,
             align_items: AlignItems::Center,
+            flex_direction: FlexDirection::Column,
+            column_gap: px(35),
             ..Default::default()
-        })
-        .with_children(|splash_title_parent| {
-            splash_title_parent.spawn((
-                Text::new("ASTEROID"),
-                TextFont {
-                    font_size: FontSize::Px(150.),
-                    font: assets_server
-                        .load(GameFonts::ComfortaaBold.font_path())
-                        .into(),
-                    ..Default::default()
-                },
-                TextColor(Color::WHITE),
-            ));
-        });
+        },
+        SplashScreenComponent,
+        children![splash_title(&assets_server), start_button(&assets_server)],
+    ));
+}
+
+fn splash_title(assets_server: &AssetServer) -> impl Bundle {
+    (
+        Text::new("ASTEROID"),
+        TextFont {
+            font_size: px(150).into(),
+            font: assets_server
+                .load(GameFonts::ComfortaaBold.font_path())
+                .into(),
+            ..Default::default()
+        },
+        TextColor(Color::WHITE),
+    )
+}
+
+fn start_button(assets_server: &AssetServer) -> impl Bundle {
+    (
+        Button,
+        Node {
+            width: px(200),
+            height: px(75.),
+            border: UiRect::all(px(2.5)),
+            border_radius: BorderRadius::all(px(10)),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            padding: UiRect::all(Val::Px(1.0)),
+            ..Default::default()
+        },
+        BorderColor::all(Color::WHITE),
+        BackgroundColor(Color::BLACK),
+        StartButton,
+        children![(
+            Text::new("START"),
+            TextColor(Color::WHITE),
+            TextFont {
+                font: assets_server
+                    .load(GameFonts::ComfortaaMedium.font_path())
+                    .into(),
+                font_size: px(30).into(),
+                ..Default::default()
+            },
+        )],
+    )
+}
+
+fn button_interactions(
+    mut game_state: ResMut<NextState<GameState>>,
+    interaction_query: Query<
+        (&mut Button, &Interaction),
+        (Changed<Interaction>, With<StartButton>),
+    >,
+) {
+    for (button, interactions) in interaction_query {
+        match *interactions {
+            Interaction::Pressed => {
+                game_state.set(GameState::Game);
+            }
+            _ => (),
+        }
+    }
+}
+
+fn clean_up_splash_screen(
+    mut commands: Commands,
+    splash_screen_entity: Single<Entity, With<SplashScreenComponent>>,
+) {
+    commands.entity(*splash_screen_entity).despawn();
 }
