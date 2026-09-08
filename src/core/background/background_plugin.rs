ca77495e640b89f37e81912e9cd836b83356d4d8
Vaaris5 <vaarischitkara16@gmail.com>
2026-09-08 22:24:48 +0530

    added a new stary background and removed unused fonts

--- src/core/background/background_plugin.rs
+++ src/core/background/background_plugin.rs
@@ -1,15 +1,35 @@
-use bevy::prelude::*;
-
-use crate::BACKGROUND_COLOR;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 pub struct BackgroundPlugin;
 
 impl Plugin for BackgroundPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, set_default_bg);
+        app.add_systems(Startup, set_default_bg)
+            .add_systems(Update, resize_background);
     }
 }
 
-fn set_default_bg(mut clear_color: ResMut<ClearColor>) {
-    clear_color.0 = BACKGROUND_COLOR;
+#[derive(Component)]
+struct Background;
+
+fn set_default_bg(
+    assets_server: Res<AssetServer>,
+    window: Single<&Window, With<PrimaryWindow>>,
+    mut commands: Commands,
+) {
+    commands.spawn((
+        Sprite {
+            image: assets_server.load("star_background.png"),
+            custom_size: Some(Vec2::new(window.width(), window.height())),
+            ..Default::default()
+        },
+        Background,
+    ));
+}
+
+fn resize_background(
+    mut background: Single<&mut Sprite, With<Background>>,
+    window: Single<&Window, With<PrimaryWindow>>,
+) {
+    background.custom_size = Some(Vec2::new(window.width(), window.height()));
 }
--- src/core/game_fonts/fonts.rs
+++ src/core/game_fonts/fonts.rs
@@ -1,14 +1,10 @@
 pub enum GameFonts {
-    ComfortaaBold,
-    ComfortaaMedium,
     PressStart2P,
 }
 
 impl GameFonts {
     pub fn font_path(self) -> &'static str {
         match self {
-            GameFonts::ComfortaaBold => "fonts/Comfortaa Font/static/Comfortaa-Bold.ttf",
-            GameFonts::ComfortaaMedium => "fonts/Comfortaa Font/static/Comfortaa-Medium.ttf",
             GameFonts::PressStart2P => "fonts/Press Start 2P/PressStart2P-Regular.ttf",
         }
     }
